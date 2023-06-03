0e725c2cdf0d3df74543fe5837cb895ea9110962
Aghiles Kebaili <40153689+Arksyd96@users.noreply.github.com>
2023-06-03 18:26:40 +0200

    wandb.log on rang 0

--- modules/loggers.py
+++ modules/loggers.py
@@ -58,47 +58,48 @@ class ImageGenerationLogger(pl.Callback):
         self.every_n_epochs = every_n_epochs
         
     def on_train_epoch_end(self, trainer, pl_module):
-        if (trainer.current_epoch + 1) % self.every_n_epochs == 0:
-            # sample images
-            pl_module.eval()
-            with torch.no_grad():
-                noise = torch.randn(
-                    1,
-                    pl_module.in_channels,
-                    *pl_module.image_size
-                ).to(pl_module.device, torch.float32)
-                
-                # diffusion the noise
-                x_hat = pl_module.diffusion.sample(pl_module, noise, ddim=False, clamp=True, verbose=True)
-                x_hat = x_hat.permute(0, 2, 1, 3, 4).squeeze(0) # slices as batch, 3D to 2D
-                
-                x_hat = x_hat[::int(x_hat.shape[0] / 10), ...] # => will be of shape (10, **image_size)
-                positions = torch.arange(0, x_hat.shape[0]).to(pl_module.device, torch.long)[::int(x_hat.shape[0] / 10)]
-                
-                x_hat = x_hat.to(self.autoencoder.device, torch.float32)
-                positions = positions.to(self.autoencoder.device, torch.long)
-                
-                # decoding
-                x_hat = x_hat.type(torch.float32) # TODO: check diffusion output dtype
-                pemb = self.autoencoder.encode_position(positions)
-                if isinstance(self.autoencoder, GaussianAutoencoder):
-                    x_hat = self.autoencoder.decode(x_hat, pemb)
-                    x_hat = torch.tanh(x_hat)
-                elif isinstance(self.autoencoder, VQAutoencoder):
-                    x_hat, _, _ = self.autoencoder.decode_pre_quantization(x_hat, pemb)
-                else:
-                    raise NotImplementedError('Unknown autoencoder type')
-                
-                # at this point, the number of channels should be the number of modalities
-                # 10 x NUM_MODALITIES x H x W
-                img = torch.cat([
-                    torch.hstack([img for img in x_hat[:, idx, ...]])
-                    for idx in range(x_hat.shape[1])
-                ], dim=0)
+        if trainer.global_rank == 0:
+            if (trainer.current_epoch + 1) % self.every_n_epochs == 0:
+                # sample images
+                pl_module.eval()
+                with torch.no_grad():
+                    noise = torch.randn(
+                        1,
+                        pl_module.in_channels,
+                        *pl_module.image_size
+                    ).to(pl_module.device, torch.float32)
+                    
+                    # diffusion the noise
+                    x_hat = pl_module.diffusion.sample(pl_module, noise, ddim=False, clamp=True, verbose=True)
+                    x_hat = x_hat.permute(0, 2, 1, 3, 4).squeeze(0) # slices as batch, 3D to 2D
+                    
+                    x_hat = x_hat[::int(x_hat.shape[0] / 10), ...] # => will be of shape (10, **image_size)
+                    positions = torch.arange(0, x_hat.shape[0]).to(pl_module.device, torch.long)[::int(x_hat.shape[0] / 10)]
+                    
+                    x_hat = x_hat.to(self.autoencoder.device, torch.float32)
+                    positions = positions.to(self.autoencoder.device, torch.long)
+                    
+                    # decoding
+                    x_hat = x_hat.type(torch.float32) # TODO: check diffusion output dtype
+                    pemb = self.autoencoder.encode_position(positions)
+                    if isinstance(self.autoencoder, GaussianAutoencoder):
+                        x_hat = self.autoencoder.decode(x_hat, pemb)
+                        x_hat = torch.tanh(x_hat)
+                    elif isinstance(self.autoencoder, VQAutoencoder):
+                        x_hat, _, _ = self.autoencoder.decode_pre_quantization(x_hat, pemb)
+                    else:
+                        raise NotImplementedError('Unknown autoencoder type')
+                    
+                    # at this point, the number of channels should be the number of modalities
+                    # 10 x NUM_MODALITIES x H x W
+                    img = torch.cat([
+                        torch.hstack([img for img in x_hat[:, idx, ...]])
+                        for idx in range(x_hat.shape[1])
+                    ], dim=0)
 
-                wandb.log({
-                    'Generated examples': wandb.Image(
-                        img.detach().cpu().numpy(), 
-                        caption='Generated examples (epoch {})'.format(trainer.current_epoch)
-                    )
-                })
+                    wandb.log({
+                        'Generated examples': wandb.Image(
+                            img.detach().cpu().numpy(), 
+                            caption='Generated examples (epoch {})'.format(trainer.current_epoch)
+                        )
+                    })
